9649f4da7f0d461209e928f41868ad18619311c6
luxTutamen <kovez@protonmail.com>
2020-02-11 14:03:52 +0200

    added localistion

--- src/main/java/ua/axiom/Controller.java
+++ src/main/java/ua/axiom/Controller.java
@@ -24,22 +24,26 @@ public class Controller {
 
         String login = getInput(
                 scn,
+                resources.getString("MESSAGE.LOGIN_REQUEST_MSG"),
                 resources.getString("REGEX.LOGIN_REGEX"),
                 resources.getString("MESSAGE.CORRECT_LOGIN_INPUT_FORMAT"),
                 resources.getString("MESSAGE.WRONG_LOGIN_INPUT_FORMAT"));
         String password = getInput(
                 scn,
+                resources.getString("MESSAGE.LOGIN_REQUEST_MSG"),
                 resources.getString("REGEX.PASSWORD_REGEX"),
                 resources.getString("MESSAGE.CORRECT_PSW_INPUT_FORMAT"),
                 resources.getString("MESSAGE.WRONG_PSW_INPUT_FORMAT"));
 
     }
 
-    private String getInput(Scanner s, String regex, String correctInputMsg, String wrongInputMsg) {
+    private String getInput(Scanner scanner, String welcomeMsg, String regex, String correctInputMsg, String wrongInputMsg) {
         Pattern pattern = Pattern.compile(regex);
 
+        viewer.output(welcomeMsg);
+
         while (true) {
-            Matcher matcher = pattern.matcher(s.nextLine());
+            Matcher matcher = pattern.matcher(scanner.nextLine());
             if(matcher.matches()) {
                 viewer.output(correctInputMsg);
                 return matcher.group();
--- src/main/java/ua/axiom/Main.java
+++ src/main/java/ua/axiom/Main.java
@@ -1,9 +1,12 @@
 package ua.axiom;
 
 import java.util.ResourceBundle;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(Pattern.compile("[A-Za-z0-9_-]{4,20}").matcher("Aaasa").matches());
+
         ResourceBundle resources = ResourceBundle.getBundle("textbook_" + (args.length == 0 ? "en" : args[0]));
         Viewer viewer = new Viewer(resources);
         Model model = new Model();
